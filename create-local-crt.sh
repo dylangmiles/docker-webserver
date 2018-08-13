8a583f9e36108fde72a28498c1e449695fa814bd
dylangmiles <dylan.g.miles@gmail.com>
2018-08-13 20:34:39 +0200

    Update to gitignore in data as well as script to generate local dev certs

---
+++ create-local-crt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Development on localhost
+openssl req -x509 -out ./data/certs/localhost.crt -keyout ./data/certs/localhost.key \
+  -newkey rsa:2048 -nodes -sha256 \
+  -subj '/CN=localhost' -extensions EXT -config <( \
+   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+   
+   
+#Development with .local somains
+openssl req -x509 -out ./data/certs/local.crt -keyout ./data/certs/local.key \
+  -newkey rsa:2048 -nodes -sha256 \
+  -subj '/CN=local' -extensions EXT -config <( \
+   printf "[dn]\nCN=local\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:local\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")   
