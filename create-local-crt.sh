5de122f28c6bd2511b93bd46614b1f4da88d52db
dylangmiles <dylan.g.miles@gmail.com>
2019-07-22 12:16:59 +0200

    Lock down specific versions

--- create-local-crt.sh
+++ create-local-crt.sh
@@ -7,8 +7,9 @@ openssl req -x509 -out ./data/certs/localhost.crt -keyout ./data/certs/localhost
    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
    
    
-#Development with .local somains
+#Development with .local 
 openssl req -x509 -out ./data/certs/local.crt -keyout ./data/certs/local.key \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=local' -extensions EXT -config <( \
-   printf "[dn]\nCN=local\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:local\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")   
+   printf "[dn]\nCN=local\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:local\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+   
